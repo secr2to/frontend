74b77b03bcac2611ba498421ca238e803fdc78ec
JIINSUNG <crm03008@gmail.com>
2025-05-21 17:20:32 +0900

    Feat: 벳지형 필터 컴포넌트 구현 (#12)
    - 방 리스트 조회를 위한 뱃지형 필터 컴포넌트 구현

---
+++ secreto/entities/main/home/menuData.ts
@@ -0,0 +1,15 @@
+export const menuItems = [
+  { label: "게임", state: "game" },
+  { label: "히스토리", state: "history" },
+];
+
+export const badgeItems = [
+  {
+    label: "대기중",
+    state: "waiting",
+  },
+  {
+    label: "진행중",
+    state: "ongoing",
+  },
+];
