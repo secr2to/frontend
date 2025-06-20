b46a5d76b0eeffe49aa826c411147400a73b0371
JIINSUNG <crm03008@gmail.com>
2025-06-20 21:00:16 +0900

    Feat: 방 등록 관련 레이아웃, 자원 추가(#25)
    - 방 등록 관련 레이아웃, 자원 추가

--- secreto/app/(afterLogin)/room-regist/_layout.tsx
+++ secreto/app/(afterLogin)/room-regist/_layout.tsx
@@ -1,5 +1,18 @@
+import { useThemeColors } from "@/shared/themes/useTheme";
+import { BackButton, CustomTitle } from "@/widgets/header";
 import { Stack } from "expo-router";
 
-export default function Rayout() {
-  return <Stack screenOptions={{ headerShown: false }} />;
+export default function Layout() {
+  const theme = useThemeColors();
+  return (
+    <Stack
+      screenOptions={{
+        headerTitle: () => <CustomTitle title="방 만들기" />,
+        headerLeft: () => <BackButton />,
+        headerStyle: {
+          backgroundColor: theme.baseBackground,
+        },
+      }}
+    />
+  );
 }
