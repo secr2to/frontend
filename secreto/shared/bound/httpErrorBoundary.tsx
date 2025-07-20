99975359ef00022e954f75fae5b11ccd887f32d5
Ji Insung <49591292+JIINSUNG@users.noreply.github.com>
2025-07-21 00:00:19 +0900

    Fix: 에러바운더리 문제 수정 (#58)
    - local error와 http error에 따른 에러바운더리를 보여주지 못하던 문제 수정

--- secreto/shared/bound/httpErrorBoundary.tsx
+++ secreto/shared/bound/httpErrorBoundary.tsx
@@ -1,6 +1,7 @@
 import { ReactNode } from "react";
 import ErrorBoundary from "react-native-error-boundary";
 import ErrorFallback from "./errorFallback";
+import { HttpError } from "../error";
 
 interface HttpErrorBoundaryProps {
   children: ReactNode;
@@ -9,8 +10,14 @@ interface HttpErrorBoundaryProps {
 export default function HttpErrorBoundary({
   children,
 }: HttpErrorBoundaryProps) {
+  const handleError = (error: Error, stackTrace: string) => {
+    if (!(error instanceof HttpError)) {
+      throw error;
+    }
+  };
   return (
     <ErrorBoundary
+      onError={handleError}
       FallbackComponent={({ error, resetError }) => (
         <ErrorFallback
           content="네트워크 상태를 확인 해 주세요"
--- secreto/shared/bound/localErrorBoundary.tsx
+++ secreto/shared/bound/localErrorBoundary.tsx
@@ -1,6 +1,7 @@
 import { ReactNode } from "react";
 import ErrorBoundary from "react-native-error-boundary";
 import ErrorFallback from "./errorFallback";
+import { HttpError } from "../error";
 
 interface LocalErrorBoundaryProps {
   children: ReactNode;
@@ -9,8 +10,14 @@ interface LocalErrorBoundaryProps {
 export default function LocalErrorBoundary({
   children,
 }: LocalErrorBoundaryProps) {
+  const handleError = (error: Error, stackTrace: string) => {
+    if (error instanceof HttpError) {
+      throw error;
+    }
+  };
   return (
     <ErrorBoundary
+      onError={handleError}
       FallbackComponent={({ error, resetError }) => (
         <ErrorFallback
           content="로컬 에러가 발생했습니다."
